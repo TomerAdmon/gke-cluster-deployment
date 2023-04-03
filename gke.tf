8ed97c42269fc91dfc2fb33ce1f113d8a64af7cd
Tomer Admon <tomer.a@qualisystems.com>
2023-04-03 10:45:34 +0300

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -98,9 +98,9 @@ resource "kubernetes_secret" "gke_cluster_key" {
   
   data = {
     "kubeconfig" = templatefile("${path.module}/templates/kubeconfig-template.yaml.tpl", {
-                              context                = "${var.project_id}-gke"
-                              cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-                              endpoint               = google_container_cluster.primary.endpoint
+                              context                = "${var.project_id}-gke",
+                              cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate,
+                              endpoint               = google_container_cluster.primary.endpoint,
                               token                  = google_container_cluster.primary.master_auth.0.client_key
   }
 }
